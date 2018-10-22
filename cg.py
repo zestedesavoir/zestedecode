6c66e081904e00cd5e0e47a13a2dbcf9e54b5555
TitiAlone <titi_alone@gmx.fr>
2018-10-22 12:53:31 +0200

    Add OOP, with abstraction classes (total: 80 lines) for the user

---
+++ cg.py
@@ -0,0 +1,145 @@
+# Importation des bibliothéques pygame et dotmap (faire attention à les installer: pip install pygame dotmap)
+import pygame
+from dotmap import DotMap
+# Import de la fonction tirant des nombres aléatoires
+from random import randint
+
+# Définition de constantes de jeu par défaut
+WIDTH = 640
+HEIGHT = 400
+STEP = 32
+
+class Snake:
+	# Liste de directions pour une utilisation simplifiée
+	DIRECTIONS = DotMap({
+		"RIGHT": 0,
+		"LEFT": 2,
+		"TOP": 1,
+		"BOTTOM": 3,
+		"STOP": 4
+	})
+
+	# Liste des parties du serpent pour une plus grande aisance
+	PARTS = DotMap({
+		"HEAD": 0,
+		"BODY": 1,
+		"TAIL": 2
+	})
+
+	# initX, initY : positions initiales de la tête
+	def __init__(self, initX=(WIDTH - 32) / 2, initY=(HEIGHT - 32) / 2):
+		# Positions (x, y) du serpent sur la fenêtre de jeu
+		self.sX = [initX, initX - 32, initX - 64]
+		self.sY = [initY, initY, initY]
+		# Rotations des morceaux de serpent
+		self.sR = [0, 0, 0]
+
+	def nextFrame(self):
+		# Avancement du corps du serpent : chaque morceau prend la position du précédent
+		for i in range(len(self.sX) - 1, 0, -1):
+			self.sX[i] = self.sX[i - 1]
+			self.sY[i] = self.sY[i - 1]
+			self.sR[i] = self.sR[i - 1]
+
+	def move(self, direction, step=STEP):
+		# D'abord, bouger tout le corps
+		if direction != self.DIRECTIONS.STOP: self.nextFrame()
+
+		# Puis mettre à jour la tête selon la direction souhaitée
+		if direction == self.DIRECTIONS.RIGHT:
+			self.sX[0] += step
+		elif direction == self.DIRECTIONS.LEFT:
+			self.sX[0] -= step
+		elif direction == self.DIRECTIONS.BOTTOM:
+			self.sY[0] += step
+		elif direction == self.DIRECTIONS.TOP:
+			self.sY[0] -= step
+
+		# Les rotations sont plus simples grâce aux constantes
+		self.sR[0] = 90 * direction
+
+	def partsIterator(self):
+		# Pour chaque partie du corps...
+		for i in range(len(self.sX), 0, -1):
+			# ...on regarde d'abord le type...
+			type = self.PARTS.BODY
+			if i == 1: type = self.PARTS.HEAD
+			elif i == len(self.sX): type = self.PARTS.TAIL
+
+			# ...avant de donner les informations nécessaires.
+			yield DotMap({
+				"position": (self.sX[i - 1], self.sY[i - 1]),
+				"rotation": self.sR[i - 1],
+				"type": type
+			})
+
+	def grow(self):
+        # On regarde d'abord la direction de la dernière partie du serpent
+		direction = self.sR[len(self.sR) - 1] / 90
+
+        # On ajoute une composante de rotation similaire
+		self.sR.append(direction * 90)
+
+        # En fonction de la direction, il faut ajouter la composante à différents endroits
+		if direction == self.DIRECTIONS.RIGHT:
+			self.sX.append(self.sX[len(self.sX) - 1] - 32)
+			self.sY.append(self.sY[len(self.sY) - 1])
+		elif direction == self.DIRECTIONS.LEFT:
+			self.sX.append(self.sX[len(self.sX) - 1] + 32)
+			self.sY.append(self.sY[len(self.sY) - 1])
+		elif direction == self.DIRECTIONS.BOTTOM:
+			self.sX.append(self.sX[len(self.sX) - 1])
+			self.sY.append(self.sY[len(self.sY) - 1] - 32)
+		elif direction == self.DIRECTIONS.TOP:
+			self.sX.append(self.sX[len(self.sX) - 1])
+			self.sY.append(self.sY[len(self.sY) - 1] + 32)
+
+	def getHeadPosition(self):
+		# Donne la position de la tête du serpent
+		return (self.sX[0], self.sY[0])
+
+class Game:
+	def __init__(self, width=WIDTH, height=HEIGHT):
+		# Initialisation de PyGame
+		pygame.init()
+
+		# Ajout des variables internes
+		self.assets = { }
+		self.screen = pygame.display.set_mode((width, height))
+		self.clock = pygame.time.Clock()
+		self.wWidth = width
+		self.wHeight = height
+
+		# Un peu d'esthétique
+		# pygame.display.set_icon(apple)
+		pygame.display.set_caption("Snake")
+
+	def asset(self, name, rotation=0):
+        # Effectue une rotation (éventuelle) de la tile avant de la retourner
+		sprite = pygame.transform.rotate(self.assets[name], rotation)
+		return sprite
+
+    # Chargement d'un asset dans PyGame
+	def addAsset(self, name, path):
+		self.assets[name] = pygame.image.load(path)
+
+	def eraseScreen(self):
+		# Remplis l'écran de blanc
+		self.screen.fill((255, 255, 255))
+
+	def draw(self, sprite, position):
+		self.screen.blit(sprite, position)
+
+	def end(self):
+		# Rafraichissement de l'écran
+		pygame.display.flip()
+		# Délai avant la prochaine frame (5 FPS)
+		self.clock.tick(5)
+
+	def randomPosition(self):
+		# Donne une position sur la zone de jeu
+		return (randint(0, self.wWidth), randint(0, self.wHeight))
+
+	def collision(self, p1, p2):
+		# cf. https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+		return (p1[0] < p2[0] + 32) and (p1[0] + 32 > p2[0]) and (p1[1] < p2[1] + 32) and (p1[1] + 32 > p2[1])
--- game.py
+++ game.py
@@ -1,116 +1,83 @@
-# Import the pygame module (make sure it's installed: pip install pygame)
+# Import de la bibliothèque pygame (gestion des graphismes)
 import pygame
+# Import de la bibliothèque du coding gouter (fonctions d'abstraction)
+import cg
 
 def main():
-	# Set some window constants
-	WIDTH = 640
-	HEIGHT = 400
-	STEP = 32
+	# Création d'une instance de jeu
+	jeu = cg.Game()
+	# Création d'un serpent à l'écran
+	snake = cg.Snake()
 
-	# Default pygame initalization
-	pygame.init()
+	# Déclaration des sprites
+	jeu.addAsset("Pomme", "apple.png")
+	jeu.addAsset("Corps", "body.png")
+	jeu.addAsset("Queue", "tail.png")
+	jeu.addAsset("Tête", "head.png")
 
-	# Load the assets
-	apple = pygame.image.load("apple.png")
-	snakeHead = pygame.image.load("head.png")
-	snakeTail = pygame.image.load("tail.png")
-	snakeBody = pygame.image.load("body.png")
-
-	# Create the main game area
-	screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-	# Add a clock to control the speed of the game
-	clock = pygame.time.Clock()
-
-	# A bit of design
-	pygame.display.set_icon(apple)
-	pygame.display.set_caption("Snake")
-
-	# Variable to control the main loop
+	# Variable pour contrôler la boucle principale
 	running = True
+	# Par défaut, le serpent ne doit pas bouger
+	direction = snake.DIRECTIONS.STOP
 
-	# Store snake's position
-	snakeX = [(WIDTH - 32) / 2, (WIDTH - 32) / 2 - 32, (WIDTH - 32) / 2 - 64]
-	snakeY = [(HEIGHT - 32) / 2, (HEIGHT - 32) / 2, (HEIGHT - 32) / 2]
-	snakeR = [0, 0, 0]
+	# Stockage de la position de la pomme
+	pomme = jeu.randomPosition()
 
-	# Set step variables for the snake (do not move by default)
-	stepX = STEP
-	stepY = 0
-
-	# Main (infinite) loop
+	# Boucle principale du jeu
 	while running:
-		# Gets all events from the queue
+		# Récupération des événements de la file d'attente
 		for event in pygame.event.get():
-			# If we want to quit...
+			# Si l'utilisateur veut quitter...
 			if event.type == pygame.QUIT:
-				# ...exit the main loop, and therefore close the window
+				# ...sortie de la boucle principale, et fermeture du jeu.
 				running = False
-			# If a key is pressed
+			# Lorsqu'une touche est appuyée...
 			if event.type == pygame.KEYDOWN:
-				# Prevent diagonal movement
-				stepX = 0
-				stepY = 0
-
-				# Check the four arrows and tell the snake to move
+				# ...on vérifie les quatre flèches directionnelles pour donner au serpent sa direction.
 				if event.key == pygame.K_UP:
-					stepY = -STEP
+					direction = snake.DIRECTIONS.TOP
 				elif event.key == pygame.K_DOWN:
-					stepY = STEP
+					direction = snake.DIRECTIONS.BOTTOM
 				elif event.key == pygame.K_LEFT:
-					stepX = -STEP
+					direction = snake.DIRECTIONS.LEFT
 				elif event.key == pygame.K_RIGHT:
-					stepX = STEP
+					direction = snake.DIRECTIONS.RIGHT
 
-		# Erase the entire screen
-		screen.fill((255, 255, 255))
+		# Premièrement, effaçons l'écran
+		jeu.eraseScreen()
 
-		# Draw the snake on screen
-		for i in range(len(snakeX), 0, -1):
-			# If max, draw the tail
-			if i == len(snakeX):
-				# Rotate the snake tile
-				sprite = pygame.transform.rotate(snakeTail, snakeR[i - 1])
-				screen.blit(sprite, (snakeX[i - 1], snakeY[i - 1]))
-			# If min, draw the head
-			elif i == 1:
-				sprite = pygame.transform.rotate(snakeHead, snakeR[i - 1])
-				screen.blit(sprite, (snakeX[i - 1], snakeY[i - 1]))
+		# Dessin du serpent à l'écran
+		for part in snake.partsIterator():
+			# Choix du sprite en fonction de la partie à dessiner et rotation
+			if part.type == snake.PARTS.HEAD:
+				sprite = jeu.asset("Tête", part.rotation)
+			elif part.type == snake.PARTS.TAIL:
+				sprite = jeu.asset("Queue", part.rotation)
 			else:
-				sprite = pygame.transform.rotate(snakeBody, snakeR[i - 1])
-				screen.blit(sprite, (snakeX[i - 1], snakeY[i - 1]))
+				sprite = jeu.asset("Corps", part.rotation)
 
-		# Full screen update
-		pygame.display.flip()
+			# Dessin du sprite tourné à l'écran
+			jeu.draw(sprite, part.position)
 
-		for i in range(len(snakeX) - 1, 0, -1):
-			snakeX[i] = snakeX[i - 1]
-			snakeY[i] = snakeY[i - 1]
-			snakeR[i] = snakeR[i - 1]
+		# Dessin de la pomme
+		jeu.draw(jeu.asset("Pomme"), pomme)
 
-		# Move the snake on the two axis
-		snakeX[0] += stepX
-		snakeY[0] += stepY
+		# Terminer la phase de dessin
+		jeu.end()
 
-		# Ensure proper tiles rotation
-		if stepX > 0:
-			snakeR[0] = 0
-		elif stepX < 0:
-			snakeR[0] = 180
+		# Déplacement du serpent
+		snake.move(direction)
 
-		if stepY > 0:
-			snakeR[0] = 270
-		elif stepY < 0:
-			snakeR[0] = 90
+		if jeu.collision(snake.getHeadPosition(), pomme):
+			snake.grow()
+			pomme = jeu.randomPosition()
 
-		# Check if the snake is still on screen
-		if snakeX[0] >= (WIDTH - 32) or snakeX[0] <= 0:
-			stepX = 0
-		if snakeY[0] >= (HEIGHT - 32) or snakeY[0] <= 0:
-			stepY = 0
+		# Vérifie que le serpent soit toujours à l'écran
+		#if snakeX[0] >= (WIDTH - 32) or snakeX[0] <= 0:
+		#	stepX = 0
+		#if snakeY[0] >= (HEIGHT - 32) or snakeY[0] <= 0:
+		#	stepY = 0
 
-		# Set the speed to 5 FPS
-		clock.tick(5)
 
 
 # Run the main function only if this module is executed as the main script
