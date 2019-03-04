76238341f225cc52b8bc4808d948d0db69a69ec8
TitiAlone <titi_alone@gmx.fr>
2019-03-04 15:24:37 +0100

    Raw version of the game made with the new library

--- ateliers/snake/game.py
+++ ateliers/snake/game.py
@@ -10,19 +10,18 @@ import pygame
 # Import de la bibliothèque du coding gouter (fonctions d'abstraction)
 import cg
 
-# Tout le code à écrire sera là-dedans
-def main():
-	# Création d'une instance de jeu
-	jeu = cg.Game()
+# Fonction principale d'initialisation
+def init(jeu):
 	# Création d'un serpent à l'écran
-	snake = cg.Snake()
+	jeu.snake = cg.Snake()
 
 	# Déclaration des sprites
 	jeu.addAsset("Cactus", "assets/cactus.png")
 	# Ajout du corps
 	jeu.addAsset("Corps", "assets/body.png")
-	# Ajout queue et tête
+	# Ajout de la queue
 	jeu.addAsset("Queue", "assets/tail.png")
+	# Ajout de la tête
 	jeu.addAsset("Tête", "assets/head.png")
 	# Ajout de la pomme
 	jeu.addAsset("Pomme", "assets/apple.png")
@@ -30,104 +29,94 @@ def main():
 	# Ajout de l'asset spécial qui sera automatiquement mis en fond
 	jeu.addAsset("background", "assets/background.png")
 
-	# Variable controlant l'ouverture de la fenêtre
-	ouvert = True
 	# Par défaut, le serpent ne doit pas bouger
-	direction = snake.DIRECTIONS.STOP
+	jeu.snakeDir = jeu.snake.DIRECTIONS.STOP
 
-	# Fonction pour faire apparaitre la pomme
-	def spawnPomme():
-		# Stockage de la position de la pomme
-		pomme = jeu.randomPosition()
+	# Choix d'une position initiale pour la pomme
+	jeu.pommePos = jeu.randomApplePosition()
 
-		# Tant que la pomme est hors-zone, on la change de place
-		while jeu.isSide(pomme):
-			pomme = jeu.randomPosition()
+def boucle(jeu):
+	################################
+	###  Gestion des évenements  ###
+	################################
 
-			# Ne pas mettre la pomme sur le serpent
-			for part in snake.partsIterator():
-				if jeu.collision(pomme, part.position):
-					# Tant que la pomme est sur le joueur, on la change de place
-					pomme = jeu.randomPosition()
-
-		# Retourne la position trouvée
-		return pomme
-
-	# Ajout de la pomme
-	pomme = spawnPomme()
-
-	# Boucle principale du jeu
-	while ouvert:
-		# Récupération des événements de la file d'attente
-		for event in pygame.event.get():
-			# Si l'utilisateur veut quitter...
-			if event.type == pygame.QUIT:
-				# ...sortie de la boucle principale, et fermeture du jeu.
-				ouvert = False
-			# Lorsqu'une touche est appuyée...
-			if event.type == pygame.KEYDOWN:
-				# ...on vérifie la direction droite...
-				if event.key == pygame.K_RIGHT:
-					direction = snake.DIRECTIONS.RIGHT
-				# ...et les autres directions.
-				elif event.key == pygame.K_UP:
-					direction = snake.DIRECTIONS.TOP
-				elif event.key == pygame.K_DOWN:
-					direction = snake.DIRECTIONS.BOTTOM
-				elif event.key == pygame.K_LEFT:
-					direction = snake.DIRECTIONS.LEFT
-
-		# Effacement de l'écran, et remplissage avec les tiles de fond
-		jeu.eraseScreen()
-
-		# Dessin de tous les cactus
-		for tile in jeu.screenIterator():
-			if jeu.isSide(tile):
-				jeu.draw(jeu.asset("Cactus"), tile)
-
-		# Dessin du serpent à l'écran
-		for part in snake.partsIterator():
-			# Choix du sprite en fonction de la partie à dessiner et rotation
-			if part.type == snake.PARTS.HEAD:
-				# Rotation de la tête
-				sprite = jeu.asset("Tête", part.rotation)
-			elif part.type == snake.PARTS.TAIL:
-				sprite = jeu.asset("Queue", part.rotation)
-			else:
-				sprite = jeu.asset("Corps", part.rotation)
-
-			# Dessin du sprite à l'écran
-			jeu.draw(sprite, part.position)
-
-		# Dessin de la pomme
-		jeu.draw(jeu.asset("Pomme"), pomme)
-
-		# Ne pas oublier de terminer la phase de dessin
+	# Lorsque l'on veut quitter le jeu
+	if pygame.QUIT in jeu.events:
 		jeu.end()
 
-		# Déplacement du serpent
-		snake.move(direction)
+	# Lorsqu'une touche est appuyée
+	if pygame.KEYDOWN in jeu.events:
+		# On stocke la touche appuyée
+		touche = jeu.events[pygame.KEYDOWN]
 
-		# Si il y a collision entre la pomme et la tête du serpent
-		if jeu.collision(snake.getHeadPosition(), pomme):
-			# Le serpent grandit
-			snake.grow()
-			# La pomme change de position
-			pomme = spawnPomme()
+		# ...on vérifie la direction droite...
+		if touche == pygame.K_RIGHT:
+			jeu.snakeDir = jeu.snake.DIRECTIONS.RIGHT
+		# ...et les autres directions.
+		elif touche == pygame.K_UP:
+			jeu.snakeDir = jeu.snake.DIRECTIONS.TOP
+		elif touche == pygame.K_DOWN:
+			jeu.snakeDir = jeu.snake.DIRECTIONS.BOTTOM
+		elif touche == pygame.K_LEFT:
+			jeu.snakeDir = jeu.snake.DIRECTIONS.LEFT
 
-		# Vérifie que le serpent soit toujours à l'écran
-		for tile in jeu.screenIterator():
-			if jeu.isSide(tile) and jeu.collision(snake.getHeadPosition(), tile):
-				# Ferme le jeu si collision avec un mur
-				ouvert = False
+	################################
+	###     Phase de dessin      ###
+	################################
 
-		# Vérifie que le serpent ne se mord pas
-		for part in snake.partsIterator():
-			# Attention : la tête est incluse, il faut donc la retirer
-			if jeu.collision(snake.getHeadPosition(), part.position) and part.type != snake.PARTS.HEAD:
-				# Ferme le jeu si le serpent se rentre dedans
-				ouvert = False
+	# Effacement de l'écran, et remplissage avec les tiles de fond
+	jeu.eraseScreen()
 
-# Lancement de main seulement si le fichier n'est pas importé comme module
+	# Itération sur tous les morceaux de grille
+	for carreau in jeu.screenIterator():
+		# Si l'on est sur un côté...
+		if jeu.isSide(carreau):
+			# ...dessine un cactus
+			jeu.draw("Cactus", carreau)
+
+	# Dessin du serpent à l'écran
+	for partie in jeu.snake.partsIterator():
+		# Choix du sprite en fonction de la partie à dessiner et dessin à l'écran
+		if partie.type == jeu.snake.PARTS.HEAD:
+			jeu.draw("Tête", partie.position, partie.rotation)
+		elif partie.type == jeu.snake.PARTS.TAIL:
+			jeu.draw("Queue", partie.position, partie.rotation)
+		else:
+			jeu.draw("Corps", partie.position, partie.rotation)
+
+	# Dessin de la pomme
+	jeu.draw("Pomme", jeu.pommePos)
+
+	# Ne pas oublier de terminer la phase de dessin
+	jeu.endDraw()
+
+	################################
+	### Déplacement et collision ###
+	################################
+
+	# Déplacement du serpent
+	jeu.snake.move(jeu.snakeDir)
+
+	# Si il y a collision entre la pomme et la tête du serpent
+	if jeu.collision(jeu.snake.getHeadPosition(), jeu.pommePos):
+		# Le serpent grandit
+		jeu.snake.grow()
+		# La pomme change de position
+		jeu.pommePos = jeu.randomApplePosition()
+
+	# Vérifie que le serpent soit toujours à l'écran
+	for carreau in jeu.screenIterator():
+		if jeu.isSide(carreau) and jeu.collision(jeu.snake.getHeadPosition(), carreau):
+			# Ferme le jeu si collision avec un mur
+			jeu.end()
+
+	# Vérifie que le serpent ne se mord pas
+	for partie in jeu.snake.partsIterator():
+		# Attention : la tête est incluse, il faut donc la retirer
+		if jeu.collision(jeu.snake.getHeadPosition(), partie.position) and partie.type != jeu.snake.PARTS.HEAD:
+			# Ferme le jeu si le serpent se rentre dedans
+			jeu.end()
+
+# Lancement du jeu seulement si le fichier n'est pas importé comme module
 if __name__ == "__main__":
-	main()
+	cg.Game(init, boucle)
